8f7ec746af62a38c68c31e63e8a2e78ade27019a
Rasim Mehdiyev <rasim.mehdiyev@student.kuleuven.be>
2024-05-04 16:30:53 +0200

    fixed viewcontroller.java

--- src/main/java/org/bundlebite/bundlebite2/BundleBite2Application.java
+++ src/main/java/org/bundlebite/bundlebite2/BundleBite2Application.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan(basePackages = {"org.bundlebite.bundlebite2"})
 @SpringBootApplication
 public class BundleBite2Application {
 
