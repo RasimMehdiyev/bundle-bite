a3afc191becf7699fe494aa9cd359238ef6e6f66
penrekkas <penrekkas@gmail.com>
2024-05-09 21:56:57 +0200

    users page in progress

---
+++ src/main/bundlebite-front/src/components/UserCardComponent.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import UserModalComponent from "./UserModalComponent";
+
+
+const UserCardComponent = ({
+      userId,
+      name,
+      email,
+      }) => {
+
+
+
+    return(
+
+        <div class="user-card">
+              <p className="user-card-header">
+                    USER #{userId}
+              </p>
+
+              <div className="user-card-content">
+                  <div className="user-details">
+                      <p> {name} </p>
+                      <p>{email} </p>
+                  </div>
+                  <button>SEE ORDERS</button>
+              </div>
+
+        </div>
+
+    )
+}
+
+
+export default UserCardComponent;
--- src/main/bundlebite-front/src/pages/AllUsers.jsx
+++ src/main/bundlebite-front/src/pages/AllUsers.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ManagerSidebar from "../components/ManagerSidebar.jsx";
+import UserCardComponent from "../components/UserCardComponent.jsx";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const AllUsers = () => {
     return(
         <div className="all">
             <ManagerSidebar activeLink="/all-users"/>
             <div className="customer-list">
-                <h1 className="header">CUSTOMERS</h1>
+                <div className="header-container">
+                    <h1 className="header">CUSTOMERS</h1>
+                    <div className="search-container">
+                        <FontAwesomeIcon icon={faSearch} className="search-icon" />
+                        <input className="search-bar" type="text" placeholder="#USER"/>
+                    </div>
+                </div>
+                <UserCardComponent userId="25001976" name="Jane Doe" email="janedoe@gmail.com"/>
+                <UserCardComponent userId="25001976" name="Jane Doe" email="janedoe@gmail.com"/>
+
             </div>
         </div>
     )
