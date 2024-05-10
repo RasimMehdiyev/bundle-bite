3af8ad19322bde4538e7e2284da486dc1f06f073
Rasim Mehdiyev <rasim.mehdiyev@student.kuleuven.be>
2024-05-10 11:54:34 +0200

    All Orders API

--- src/main/java/org/bundlebite/bundlebite2/FirebaseInit.java
+++ src/main/java/org/bundlebite/bundlebite2/FirebaseInit.java
@@ -14,21 +14,32 @@ import java.io.InputStream;
 import java.util.HashMap;
 import com.google.firebase.auth.FirebaseAuthException;
 import java.util.Map;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Base64;
+// import java.util.Base64.Decoder;
 
 @Component
 public class FirebaseInit {
     private static final Logger logger = LoggerFactory.getLogger(FirebaseInit.class);
     @PostConstruct
     public void initialize() {
+        Dotenv dotenv = Dotenv.load();
+        String json = dotenv.get("FIREBASE_CREDENTIALS_BASE64");
+        byte[] decodedJson = java.util.Base64.getDecoder().decode(json);
+        String databaseURL = dotenv.get("DATABASE_URL");
+        ByteArrayInputStream serviceAccountStream = new ByteArrayInputStream(decodedJson);
+
+
         try {
             logger.info("Initializing Firebase...");
-            InputStream serviceAccount = this.getClass().getClassLoader().getResourceAsStream("./bundle-bite-firebase-adminsdk-sz26n-016d7718b6.json");
+            // InputStream serviceAccount = this.getClass().getClassLoader().getResourceAsStream("./bundle-bite-firebase-adminsdk-sz26n-016d7718b6.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
                     .setDatabaseUrl("https://bundle-bite.europe-west1.firebaseio.com")
                     .build();
 
