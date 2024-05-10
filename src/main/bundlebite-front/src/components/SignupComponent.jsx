15aefe70418535cdee75fe4e731863cde4611109
penrekkas <penrekkas@gmail.com>
2024-05-10 18:00:46 +0200

    Sign up page improved

--- src/main/bundlebite-front/src/components/SignupComponent.jsx
+++ src/main/bundlebite-front/src/components/SignupComponent.jsx
@@ -15,10 +15,10 @@ const SignupComponent = () => {
         <div className="auth-main">
             <h1 className="login-header">SIGN UP</h1>
             <form className="auth" onSubmit={handleSubmit}>
-                <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required placeholder="Email"></input>
-                <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} required placeholder="Password"></input>
-                <input type="password" id="confirm-password" name="confirm-password" required placeholder="Confirm password"></input>
-                <button type="submit">SAVE</button>
+                <input type="email" id="email" name="email" value={email} style={{ textAlign: 'left', borderRadius: '15px'}} onChange={e => setEmail(e.target.value)} required placeholder="Email"></input>
+                <input type="password" id="password" name="password" value={password} style={{ textAlign: 'left', borderRadius: '15px'}} onChange={e => setPassword(e.target.value)} required placeholder="Password"></input>
+                <input type="password" id="confirm-password" name="confirm-password" style={{ textAlign: 'left', borderRadius: '15px'}} required placeholder="Confirm password"></input>
+                <button type="submit" style={{ marginTop: '30px', width: '400px', height: '60px', fontFamily:'Inter', borderRadius: '15px', fontSize:'25px'}}>SAVE</button>
             </form>
         </div>
     )
