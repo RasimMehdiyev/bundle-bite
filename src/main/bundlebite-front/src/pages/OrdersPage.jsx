3542e9d70678c200301c5b29611763f3ce533f5d
ChristianaGalani <christilightblue@gmail.com>
2024-06-15 17:32:50 +0200

    Adding Product IDs

--- src/main/bundlebite-front/src/pages/OrdersPage.jsx
+++ src/main/bundlebite-front/src/pages/OrdersPage.jsx
@@ -17,14 +17,14 @@ const OrdersPage = () => {
     
     var [cards, setCards] = useState([
         { id: 0, quantity: 0 , name:"SOUVLAKI", img:process.env.PUBLIC_URL + "/images/design/souvlaki.png", price:6}, // Initial state for card 1
-        { id: 1, quantity: 0, name:"VODKA PASTA", img:process.env.PUBLIC_URL + "/images/design/pasta.png", price:8}, // Initial state for card 2
-        { id: 2, quantity: 0, name: "PIZZA MARGHERITA", img:process.env.PUBLIC_URL + "/images/design/pizza.png", price:7}    ,
+        { id: "BundleBite/kw35uS6JfAqZOeHZyO2C ", quantity: 0, name:"VODKA PASTA", img:process.env.PUBLIC_URL + "/images/design/pasta.png", price:8}, // Initial state for card 2
+        { id: "BundleBite/iVWXKXlFqnYdQO9PT41m", quantity: 0, name: "PIZZA MARGHERITA", img:process.env.PUBLIC_URL + "/images/design/pizza.png", price:7}    ,
         { id: 3, quantity: 0, name: "BURRITO", img:process.env.PUBLIC_URL + "/images/design/burrito.png", price:10},
         { id: 4, quantity: 0, name: "POKE BOWL", img:process.env.PUBLIC_URL + "/images/design/poke.png", price:12},
         { id: 5, quantity: 0, name: "TACO", img:process.env.PUBLIC_URL + "/images/design/taco.png", price:9},
-        { id: 6, quantity: 0, name: "VOL-AU-VENT", img:process.env.PUBLIC_URL + "/images/design/volauvent.png", price:11},
+        { id: "BundleBite/fXlirxyJhKYeHRABD7Tj", quantity: 0, name: "VOL-AU-VENT", img:process.env.PUBLIC_URL + "/images/design/volauvent.png", price:11},
         { id: 7, quantity: 0, name: "CHILI CON CARNE", img:process.env.PUBLIC_URL + "/images/design/chili.png", price:13},
-        { id: 8, quantity: 0, name: "SHAH PILAF", img:process.env.PUBLIC_URL + "/images/design/shah.png", price:14},
+        { id: "BundleBite/QmKJGOjroos8Sa45tpyt" , quantity: 0, name: "SHAH PILAF", img:process.env.PUBLIC_URL + "/images/design/shah.png", price:14},
         { id: 9, quantity: 0, name: "SPANAKOPITA", img:process.env.PUBLIC_URL + "/images/design/spanakopita.png", price:15}
     ]);
 
@@ -244,7 +244,7 @@ const OrdersPage = () => {
             //id:0,
             quantity: card.quantity,
             name: moreInfo.name,
-            img: '"/images/design/' + moreInfo.img,
+            img: '/images/design/' + moreInfo.img,
             price: moreInfo.price
         }
         console.log(newCard);
